5895a449adddef885db4abcb285ebb99e3966a46
Kyeong Hoon Chu <khchu1220@naver.com>
2025-06-15 12:10:04 +0900

    feat: config 파일 추가

---
+++ components/common/IconButton.tsx
@@ -0,0 +1,20 @@
+import { cn } from '@/utils/cn';
+
+export default function IconButton({
+  className,
+  type = 'button',
+  ...props
+}: React.ComponentProps<'button'>) {
+  return (
+    <button
+      aria-label="icon-button"
+      {...props}
+      type={type}
+      className={cn(
+        'flex h-9 w-9 items-center justify-center rounded-lg transition-all',
+        'text-secondary hover:bg-secondary',
+        className,
+      )}
+    />
+  );
+}
