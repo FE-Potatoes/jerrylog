7901304c7b4d7874b529407ddf0b1f87a4de190f
Kyeong Hoon Chu <khchu1220@naver.com>
2025-06-26 21:09:44 +0900

    feat: Home 최신 Post 기능 추가

--- constants/dataset.ts
+++ constants/dataset.ts
@@ -1,7 +1,18 @@
+import { PostMeta } from '@/types/blogType';
 import { readdir } from 'fs/promises';
 import path from 'path';
 
-export async function getPosts(type: 'dev' | 'life') {
+// 최신 날짜 기준으로 정렬
+export const calSortTimePosts = (posts: PostMeta[]) => {
+  const sortTimePost = posts.sort(
+    (a, b) => new Date(b.date).getTime() - +new Date(a.date).getTime(),
+  );
+
+  return sortTimePost;
+};
+
+// 특정 게시판 게시물 받기
+export async function calGetPosts(type: 'dev' | 'life') {
   // 파일 경로 찾기
   const postPath = path.resolve(process.cwd(), 'public', 'blog', `${type}`);
   const fileList = await readdir(postPath, { withFileTypes: true });
@@ -10,14 +21,21 @@ export async function getPosts(type: 'dev' | 'life') {
   const posts = await Promise.all(
     fileList.map(async ({ name }) => {
       const { metadata } = await import(`@/public/blog/${type}/${name}`);
-      return { name, ...metadata };
+      const transformName = name.replace('.mdx', '');
+
+      return { ...metadata, name: transformName };
     }),
   );
 
-  // 최신 날짜 기준으로 정렬
-  const sortTimePost = posts.sort(
-    (a, b) => new Date(b.date).getTime() - +new Date(a.date).getTime(),
-  );
+  return posts;
+}
 
-  return sortTimePost;
+// 모든 Post 반환
+export async function calGetAllPosts() {
+  const [devPosts, lifePosts] = await Promise.all([
+    calGetPosts('dev'),
+    calGetPosts('life'),
+  ]);
+  const allPosts = [...devPosts, ...lifePosts];
+  return allPosts;
 }
---
+++ types/blogType.ts
@@ -0,0 +1,11 @@
+export type PostMeta = {
+  author: string;
+  category: 'dev' | 'life';
+  date: string;
+  description: string;
+  icon?: string;
+  image: string;
+  name: string;
+  tags: string[];
+  title: string;
+};
