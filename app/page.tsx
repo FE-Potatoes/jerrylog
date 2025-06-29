5939ad8121a3c64a05e31ba467925b0c1da76795
Kyeong Hoon Chu <khchu1220@naver.com>
2025-06-29 15:16:55 +0900

    style: Themebutton 깜빡임 animation으로 변경 및 폰트 깜빡임 변경

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,15 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col">
-      <h1 className="mb-4">경훈 ﹒ JerryChu</h1>
-      <div className="font-arita text-secondary mb-8">
-        <p>
+      <h1 className="font-caveat mb-4">
+        <span className="font-arita">경훈</span> ﹒ JerryChu
+      </h1>
+      <div className="text-secondary mb-8">
+        <p className="font-arita">
           화려하기보다는 직관적인 UI, 복잡하기보다는 이해하기 쉬운 코드를
           지향합니다.
         </p>
-        <p>
+        <p className="font-arita">
           개발하면서 배운 것들을 기록하고, 가끔은 일상의 경험도 함께 기록합니다.
           꾸준히 배우고 천천히 나아가는 중입니다.
         </p>
