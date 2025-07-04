9cc967a9e86bc926be959bbf9e8f6c92a3cdc389
Kyeong Hoon Chu <khchu1220@naver.com>
2025-07-04 19:39:58 +0900

    style: tag border 추가

--- components/common/Tag.tsx
+++ components/common/Tag.tsx
@@ -7,7 +7,7 @@ export default function Tag({
 }) {
   return (
     <button
-      className="bg-thirdary font-pretandard hover:text h-fit w-fit shrink-0 cursor-pointer rounded-lg px-2 py-0.5 text-xs text-nowrap transition-colors hover:bg-gray-300"
+      className="bg-thirdary border-thirdary font-pretandard hover:text h-fit w-fit shrink-0 cursor-pointer rounded-lg border-1 px-2 py-0.5 text-xs text-nowrap transition-colors hover:bg-gray-300"
       onClick={onclick}
       aria-label={`${children}로 필터링`}
     >
