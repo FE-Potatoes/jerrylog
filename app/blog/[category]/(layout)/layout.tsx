3ea0c8c72c7bd3521073cb9ebe11f0f9d67402d6
Kyeong Hoon Chu <khchu1220@naver.com>
2025-07-05 01:09:48 +0900

    fix: url canonical 개별 설정

--- app/blog/[category]/(layout)/layout.tsx
+++ app/blog/[category]/(layout)/layout.tsx
@@ -13,9 +13,9 @@ export async function generateMetadata({
 
   return {
     alternates: {
-      canonical: siteConfig.url,
+      canonical: `${siteConfig.url}/blog/${category}`,
       languages: {
-        'ko-KR': siteConfig.url,
+        'ko-KR': `${siteConfig.url}/blog/${category}`,
       },
     },
     title: metaTitle,
