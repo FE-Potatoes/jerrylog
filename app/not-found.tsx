f390b076ed2067456816ffa48b900a8e88bb787d
Kyeong Hoon Chu <khchu1220@naver.com>
2025-07-24 12:32:03 +0900

    fix: import 경로 수정

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import ErrorButton from '@/shared/ui/ErrorButton';
+import { ErrorButton } from '@/shared/ui';
 import { useRouter } from 'next/navigation';
 
 export default function NotFoundPage() {
