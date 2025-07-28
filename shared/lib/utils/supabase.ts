e508f6e23c8a68f6cca9f6c79debc213508bd6cc
Kyeong Hoon Chu <khchu1220@naver.com>
2025-07-28 16:39:37 +0900

    fix: 민감한 정보 next_public 제거

--- shared/lib/utils/supabase.ts
+++ shared/lib/utils/supabase.ts
@@ -1,6 +1,6 @@
 import { Database } from '@/types_db';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY as string;
+const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseKey = process.env.SUPABASE_API_KEY as string;
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
