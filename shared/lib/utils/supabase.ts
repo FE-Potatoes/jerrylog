aecccff9762806bf19de1ac0cdd85d9019713793
Kyeong Hoon Chu <khchu1220@naver.com>
2025-07-29 11:45:56 +0900

    feat: supabase fetch no-store 처리

--- shared/lib/utils/supabase.ts
+++ shared/lib/utils/supabase.ts
@@ -3,4 +3,12 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL as string;
 const supabaseKey = process.env.SUPABASE_API_KEY as string;
-export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
+  global: {
+    fetch: (input: RequestInfo | URL, init?: RequestInit | undefined) =>
+      fetch(input, {
+        ...init,
+        cache: 'no-store',
+      }),
+  },
+});
